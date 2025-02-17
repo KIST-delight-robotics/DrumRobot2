a50a335ed536389367d5314692e75945f14c10b5
hynsxo <hyunseo379@hanmail.net>
2025-02-17 10:49:22 +0900

    feature : 손목모터에 1ms마다 핑 보내주기

--- src/DrumRobot.cpp
+++ src/DrumRobot.cpp
@@ -205,6 +205,23 @@ void DrumRobot::recvLoopForThread()
     {
         recv_time_point = std::chrono::steady_clock::now();
         recv_time_point += std::chrono::microseconds(100);  // 주기 : 100us
+        
+        // 손목 모터에 1ms마다 핑 보내주기
+        auto currentTime = chrono::system_clock::now();
+        auto elapsedTimeMaxon = chrono::duration_cast<chrono::microseconds>(currentTime - SendMaxon);
+        if(elapsedTimeMaxon.count() >= 1000) {
+            for (auto &motorPair : motors)
+            {
+                if (maxonMotorCount != 0)
+                {
+                    if (std::shared_ptr<MaxonMotor> maxonMotor = std::dynamic_pointer_cast<MaxonMotor>(motorPair.second))
+                    {
+                        maxoncmd.getCheck(*maxonMotor, &virtualMaxonMotor->sendFrame);
+                    }
+                }
+            }
+            SendMaxon = currentTime;
+        }
 
         switch (state.main.load())
         {
