713468b2841e167ebc516a8ef4c64f76ad17ddbd
taehwang1022 <taehwang_kim@naver.com>
2025-03-20 10:02:18 +0900

    reload

--- src/DrumRobot.cpp
+++ src/DrumRobot.cpp
@@ -605,8 +605,6 @@ void DrumRobot::sendLoopForThread()
         }
         if (allMotorsStagnant)
         {
-            std::cout << "지금 자는중임 " << "\n";
-            usleep(50000);
             flagObj.setFixationFlag("fixed");
         }
 
