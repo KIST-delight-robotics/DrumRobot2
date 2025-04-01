f74e156030363a63d966d19eda9ef385e74a7174
taehwang1022 <taehwang_kim@naver.com>
2025-04-01 15:10:27 +0900

    parseMatrix

--- src/PathManager.cpp
+++ src/PathManager.cpp
@@ -659,6 +659,54 @@ void PathManager::parseMeasure(MatrixXd &measureMatrix)
     measureMatrix = tmpMatrix;
 }
 
+MatrixXd PathManager::parseMatrix(MatrixXd &measureMatrix)
+{
+    double timeStep = 0.05
+    int numCols = measureMatrix.cols();
+    std::vector<Eigen::VectorXd> newRows;
+
+    double prevTotalTime = 0.0;
+
+    for (int i = 0; i < measureMatrix.rows(); ++i)
+    {
+        double duration = measureMatrix(i, 1);
+        double endTime = measureMatrix(i, 8);
+        int steps = static_cast<int>(round(duration / timeStep));
+        double sumStep = (endTime-prevTotalTime) / steps;
+
+        for (int s = 1; s <= steps; ++s)
+        {
+            Eigen::VectorXd row(numCols);
+            row.setZero();
+
+            row(0) = measureMatrix(i, 0);                 
+            row(1) = timeStep;                              
+            row(8) = prevTotalTime + s * sumStep;          
+
+            if (s == steps)
+            {
+                for (int j = 2; j <= 7; ++j)
+                {
+                    row(j) = measureMatrix(i, j);
+                }
+            }
+
+            newRows.push_back(row);
+        }
+
+        prevTotalTime = endTime;
+    }
+
+    Eigen::MatrixXd parsedMatrix(newRows.size(), numCols);
+    for (size_t i = 0; i < newRows.size(); ++i)
+    {
+        parsedMatrix.row(i) = newRows[i];
+    }
+
+    return parsedMatrix;
+
+}
+
 pair<VectorXd, VectorXd> PathManager::parseOneArm(VectorXd t, VectorXd inst, VectorXd stateVector)
 {
     map<int, int> instrumentMapping = {
